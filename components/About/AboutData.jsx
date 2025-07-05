8a219879b36e19bd4fa29a2ec2c80e9e0e12179d
mateuszniznik8 <streetvers.game@gmail.com>
2025-07-04 19:10:04 -0700

    Initial commit

---
+++ components/About/AboutData.jsx
@@ -0,0 +1,42 @@
+import HeroAvatar from '@/public/images/hero-avatar.jpg';
+
+export const aboutData = {
+    mainData: {
+        heroAvatar: HeroAvatar,
+        biography: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        projectsDone: "432",
+        yearsOfExperience: "13",
+        worldwideClients: "900",
+    },
+    skills: [
+        {
+            name: 'Web Development',
+        },
+        {
+            name: 'UI & UX Design',
+        },
+        {
+            name: 'Marketing',
+        },
+        {
+            name: 'SEO',
+        },
+        {
+            name: 'Management',
+        },
+    ],
+    connect: [
+        {
+            url: '#',
+            bootstrapIcon: 'bi bi-facebook',
+        },
+        {
+            url: '#',
+            bootstrapIcon: 'bi bi-twitter',
+        },
+        {
+            url: '#',
+            bootstrapIcon: 'bi bi-instagram',
+        },
+    ]
+};
---
+++ components/Services/ServicesData.jsx
@@ -0,0 +1,27 @@
+export const servicesData = {
+    mainData: {
+        title: "Services",
+        title2: "What I",
+        title2Span: "Do",
+    },
+    services: [
+        {
+            number: '01',
+            bootstrapIcon: 'bi bi-brush',
+            title: 'Design',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut',
+        },
+        {
+            number: '02',
+            bootstrapIcon: 'bi bi-code-slash',
+            title: 'Development',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et',
+        },
+        {
+            number: '03',
+            bootstrapIcon: 'bi bi-laptop',
+            title: 'Marketing',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore',
+        },
+    ]
+};
