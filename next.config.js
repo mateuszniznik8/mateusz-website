f063559361031c253b6695266e3b552f65f0d7c8
Mateusz <streetvers.game@gmail.com>
2025-07-07 00:48:20 -0700

    Allow placeholder image domain

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const path = require('path')
 
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['placehold.co'],
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
